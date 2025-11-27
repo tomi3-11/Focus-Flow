f620eafd02ff54ad9a44c0780fdee6e4053d3072
tom <didymusthomas6@gmail.com>
2025-11-27 20:58:38 +0300

    Updated environmental variables

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     # Database configurations
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key-here'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATION = False
     REMEMBER_COOKIE_DURATION = timedelta(days=7)
+
